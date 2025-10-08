f358fe5c4bc453115e7d6a5bf8b6aeff95e9980b
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-10-08 20:15:51 +0530

    Time: 0 ms (100%), Space: 53.2 MB (85.11%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] !== 9) {
+            digits[i]++;
+            return digits;
+        }else{
+            digits[i] = 0;
+        }
+    }
+
+    digits = new Array(digits.length + 1).fill(0);
+    digits[0] =1;
+    return digits;
+};
