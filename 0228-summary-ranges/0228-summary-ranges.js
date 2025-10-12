d98d82540ca5ba09458b25c92b23e835d34a629e
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-10-12 11:22:27 +0530

    Time: 0 ms (100%), Space: 53.1 MB (84.49%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    
+    let result = []
+
+    for(let i= 0; i < nums.length; i++){
+        let start = nums[i];
+
+        while(i+1 < nums.length && nums[i+1] === nums[i] +1){
+            i++;
+        }
+
+        if(start === nums[i]){
+            result.push(`${start}`)
+        }else{
+            result.push(`${start}->${nums[i]}`);
+        }
+    }
+    return result;
+};
