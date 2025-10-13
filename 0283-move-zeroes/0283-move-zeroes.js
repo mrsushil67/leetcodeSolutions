8433fee37e36917a49b3700cffe728876a6471db
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-10-13 20:31:55 +0530

    Time: 2 ms (53.54%), Space: 59.6 MB (72.5%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let suggestedLastIndex = 0;
+
+    for(let i = 0;i < nums.length; i++){
+        if(nums[i]!==0){
+            nums[suggestedLastIndex] = nums[i];
+            suggestedLastIndex++;
+        }
+    };
+
+    for(let j = suggestedLastIndex; j < nums.length; j ++){
+        nums[j] = 0;
+    }
+}
