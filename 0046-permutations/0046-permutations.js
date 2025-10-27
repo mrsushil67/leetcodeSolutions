e386f5edf384abf127370830e4f406d8636f1ec2
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-10-27 18:25:36 +0530

    Time: 0 ms (100%), Space: 60.2 MB (29.77%) - LeetHub

---
+++ 0046-permutations/0046-permutations.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+function permute(nums) {
+    const res = [];
+    const used = new Array(nums.length).fill(false);
+
+    function backtrack(path) {
+        if (path.length === nums.length) {
+            res.push([...path]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            path.push(nums[i]);
+            backtrack(path);
+            path.pop();
+            used[i] = false;
+        }
+    }
+
+    backtrack([]);
+    return res;
+}
