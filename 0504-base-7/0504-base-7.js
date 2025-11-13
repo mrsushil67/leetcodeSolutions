d7aed0ebf36a0de4e8f0b5dae63166093b62a045
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-11-13 23:14:19 +0530

    Time: 0 ms (100%), Space: 54 MB (73.18%) - LeetHub

---
+++ 0504-base-7/0504-base-7.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+    if (num === 0) return "0";
+
+    const isNegative = num < 0;
+    num = Math.abs(num);
+    
+    let result = '';
+
+    while (num > 0) {
+        result = (num % 7) + result;
+        num = Math.floor(num / 7);
+    }
+
+    if (isNegative) {
+        result = '-' + result;
+    }
+
+    return result;
+}
