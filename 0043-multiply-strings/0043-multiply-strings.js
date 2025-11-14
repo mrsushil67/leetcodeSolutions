f66a55fcf2aa2360584c3fba80e3fb7eb59e777b
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-11-14 21:26:22 +0530

    Time: 8 ms (38.25%), Space: 57.3 MB (22.95%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    if (num1 === "0" || num2 === "0") return "0";
+    
+    const m = num1.length, n = num2.length;
+    const result = new Array(m + n).fill(0);
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            const mul = (num1[i] - '0') * (num2[j] - '0');
+            const sum = mul + result[i + j + 1];
+            result[i + j + 1] = sum % 10;
+            result[i + j] += Math.floor(sum / 10);
+        }
+    }
+
+    let resultStr = '';
+    for (let i = 0; i < result.length; i++) {
+        if (!(resultStr === '' && result[i] === 0)) {
+            resultStr += result[i];
+        }
+    }
+
+    return resultStr;
+}
