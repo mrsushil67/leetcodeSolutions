36db394825af00a6e873116c46e7c0c6d6e800ba
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-12-05 22:36:10 +0530

    Time: 0 ms (100%), Space: 54.9 MB (61.7%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    const n = matrix.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        let left = 0;
+        let right = n - 1;
+
+        while (left < right) {
+            let temp = matrix[i][left];
+            matrix[i][left] = matrix[i][right];
+            matrix[i][right] = temp;
+            left++;
+            right--;
+        }
+    }
+};
