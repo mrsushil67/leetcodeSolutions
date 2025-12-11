6f4f4499be6f7cbb892c9cb371536361fe2c90b4
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-12-11 22:47:24 +0530

    Time: 42 ms (98.22%), Space: 56.6 MB (59.8%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function(s1, s2, s3) {
+    if (s1.length + s2.length !== s3.length) {
+        return false;
+    }
+
+    let dp = [];
+    for (let i = 0; i <= s1.length; i++) {
+        dp[i] = [];
+        for (let j = 0; j <= s2.length; j++) {
+            dp[i][j] = false;
+        }
+    }
+
+    dp[0][0] = true;
+
+    for (let i = 0; i <= s1.length; i++) {
+        for (let j = 0; j <= s2.length; j++) {
+            if (i > 0 && s1[i - 1] === s3[i + j - 1]) {
+                dp[i][j] = dp[i][j] || dp[i - 1][j];
+            }
+            if (j > 0 && s2[j - 1] === s3[i + j - 1]) {
+                dp[i][j] = dp[i][j] || dp[i][j - 1];
+            }
+        }
+    }
+
+    return dp[s1.length][s2.length];
+}
