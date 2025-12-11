74beb326ce0538a5bbc966a4dff76872a3f964ad
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2025-12-11 22:53:01 +0530

    Time: 28 ms (59.53%), Space: 78.5 MB (67.29%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    if (!head || !head.next) return head;
+
+    let slow = head, fast = head, prev = null;
+    while (fast && fast.next) {
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    prev.next = null;
+
+    let l1 = sortList(head);
+    let l2 = sortList(slow);
+
+    return merge(l1, l2);
+}
+
+function merge(l1, l2) {
+    let dummy = { val: 0, next: null };
+    let cur = dummy;
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            cur.next = l1;
+            l1 = l1.next;
+        } else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+        cur = cur.next;
+    }
+
+    cur.next = l1 || l2;
+    return dummy.next;
+}
